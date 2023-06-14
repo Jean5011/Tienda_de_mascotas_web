be9c47e6b5bbeaa891ba1ca43b8f37210c830ca0
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-06-14 17:26:42 -0300

    Asignación de propiedades

---
+++ Entidades/Proveedor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades {
+    class Proveedor {
+        public string CUIT { get; set; }
+        public string RazonSocial { get; set; }
+        public string NombreContacto { get; set; }
+        public string CorreoElectronico { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public string Localidad { get; set; }
+        public string Provincia { get; set; }
+        public string Pais { get; set; }
+        public string CodigoPostal { get; set; }
+    }
+}
