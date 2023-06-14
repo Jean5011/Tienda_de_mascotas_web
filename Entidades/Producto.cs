cdb297107de7c144a7f1b3db36644696f614003d
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-06-14 18:34:05 -0300

    Creación de entidades y estructura

---
+++ Entidades/Producto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades {
+    class Producto {
+        public int Codigo { get; set; }
+        public Proveedor Proveedor { get; set; }
+        public TipoProducto TipoProducto { get; set; }
+        public string Nombre { get; set; }
+        public string Marca { get; set; }
+        public string Descripcion { get; set; }
+        public int Cantidad { get; set; }
+        public string Imagen { get; set; }
+        public double Precio { get; set; }
+        public bool Estado { get; set; }
+
+    }
+}
---
+++ Entidades/TipoProducto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades {
+    class TipoProducto {
+        public int Id { get; set; }
+        public Animal Animal { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
