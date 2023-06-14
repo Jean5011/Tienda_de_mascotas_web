e73414630f3a515a23d5bae31b3c7f6a3b68ddff
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-06-14 19:43:30 -0300

    Creación y definición de propiedades

---
+++ Entidades/DetalleVenta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades {
+    class DetalleVenta {
+        public Venta Cabecera { get; set; } // ¿Dejamos Venta (Cabecera) o implementamos un Id?
+        public Producto Producto { get; set; }
+        public Proveedor Proveedor { get; set; } // ¿Producto no trae ya el CUIT del Proveedor?
+        public string Fecha { get; set; }
+        public int Cantidad { get; set; }
+        public double PrecioUnitario { get; set; }
+        public double PrecioTotal { get; set; }
+
+    }
+}
---
+++ Entidades/Venta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades {
+    class Venta {
+        public int Id { get; set; }
+        public Empleado EmpleadoGestor { get; set; }
+        public int TipoPago { get; set; }
+        public string Fecha { get; set; }
+        public double Total { get; set; }
+
+    }
+}
