223a97f836f4465b863dcbe125c9b02373764afd
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-06-16 19:48:44 -0300

    Solución de un problema de Referencias

---
+++ Datos/EmpleadoDatos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos {
+    public class EmpleadoDatos {
+        public static void IniciarSesion(string usuario, string clave) { }
+        public static void CrearClave(string clave) {
+
+        }
+    }
+}
--- Vista/Empleados/CambiarClave.aspx.cs
+++ Vista/Empleados/CambiarClave.aspx.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Vista.Empleados {
     public partial class CambiarClave : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
-
+            if(!IsPostBack) { }
         }
     }
 }
