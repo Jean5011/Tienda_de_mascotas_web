1518ef551bbad8be656c2d5c66ee7b5d159553de
Ezequiel Martinez <EzequielMartinezA18@gmail.com>
2023-06-15 22:34:41 -0300

    Consultas Productos y Ventas

---
+++ Consultas/CargaDeDatosProductos.sql
@@ -0,0 +1,7 @@
+use Pets
+go
+
+Insert into Productos (CodProducto_Prod,CUITProveedor_Prod,CodTipoProducto_Prod,
+Nombre_Prod,Marca_Prod,Descripcion_Prod,Stock_Prod,PrecioUnitario_Prod,Estado_Prod)
+
+SELECT 'PROD1','198753145',
