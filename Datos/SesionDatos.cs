065c80f2b4e1dd9897ca1ca08203a7889be52bb9
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-06-17 23:16:50 -0300

    Documentar funciones

--- Datos/SesionDatos.cs
+++ Datos/SesionDatos.cs
@@ -11,6 +11,11 @@ namespace Datos {
         public static string ALL_COLUMNS = $"[{Sesion.Columns.Codigo}], [{Sesion.Columns.DNI}], " +
                                            $"[{Sesion.Columns.FechaAlta}], [{Sesion.Columns.Token}], " +
                                            $"[{Sesion.Columns.Estado}]";
+        /// <summary>
+        /// Inserta un registro en la tabla Sesiones
+        /// </summary>
+        /// <param name="obj">El objeto Sesion con los datos a agregar.</param>
+        /// <returns>Response con el resultado de la operación</returns>
         public static Response AbrirSesion(Sesion obj) {
             Connection con = new Connection(Connection.Database.Pets);
             return con.Response.ErrorFound
@@ -23,6 +28,13 @@ namespace Datos {
                         }
                     );
         }
+
+        /// <summary>
+        /// Rescata los datos de una sesión a partir de un token y DNI
+        /// </summary>
+        /// <param name="token">El token en cuestión.</param>
+        /// <param name="dni">El DNI en cuestión.</param>
+        /// <returns>Response con el resultado de la operación.</returns>
         public static Response ObtenerSesion(string token, string dni) {
             Connection con = new Connection(Connection.Database.Pets);
             return con.Response.ErrorFound
@@ -35,6 +47,12 @@ namespace Datos {
                         }
                     );
         }
+
+        /// <summary>
+        /// Deshabilita un registro de la tabla Sesiones.
+        /// </summary>
+        /// <param name="obj">Objeto Sesion con los datos.</param>
+        /// <returns>Response con el resultado de la operación.</returns>
         public static Response CerrarSesion(Sesion obj) {
             Connection con = new Connection(Connection.Database.Pets);
             return con.Response.ErrorFound
@@ -46,6 +64,12 @@ namespace Datos {
                         }
                     );
         }
+
+        /// <summary>
+        /// Deshabilita todos los registros/tokens de un usuario en particular.
+        /// </summary>
+        /// <param name="obj">Objeto Sesion con el DNI del empleado especificado.</param>
+        /// <returns>Response con el resultado de la operación.</returns>
         public static Response CerrarTodasLasSesiones(Sesion obj) {
             Connection con = new Connection(Connection.Database.Pets);
             return con.Response.ErrorFound
