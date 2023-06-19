e41a06e6efbcaec0e8a6654603c4d1292548d9c5
javier torales <jtorales2016@gmail.com>
2023-06-19 19:26:05 -0300

    error del commit arreglado

--- Negocio/ProveedorNegocio.cs
+++ Negocio/ProveedorNegocio.cs
@@ -9,9 +9,8 @@ using System.Text.RegularExpressions;
 
 namespace Negocio
 {
-    class ProveedorNegociocs
+    public class ProveedorNegocio
     {
-        Proveedor Proveedor;
         public static Response ObtenerListaDeProveedores()
         {
             return ProveedorDatos.ObtenerListaDeProveedores();
@@ -36,56 +35,6 @@ namespace Negocio
             }
             
         }
-        /* public static Response ObtenerListaDeProveedores()
-         
-         public static Response ObtenerProveedorByCUIT(string CUIT)
-         {
-             string consulta = $"SELECT {ALL_COLUMNS} FROM {Proveedor.Table} WHERE [{Proveedor.Columns.CUIT}] = @cuit";
-             Connection connection = new Connection(Connection.Database.Pets);
-             return connection.Response.ErrorFound
-                 ? connection.Response
-                 : connection.FetchData(
-                         query: consulta,
-                         parameters: new Dictionary<string, object> {
-                             { "@cuit", CUIT }
-                         }
-                     );
-         }
-         public static Response InsertarProveedor(Proveedor proveedor)
-         {
-             string consulta = $"INSERT INTO {ALL_COLUMNS}" +
-                               $"VALUES (@CUIT, @RazonSocial, @NombreContacto,@CorreoElectronico,@Telefono,@Direccion,@Provincia,@Localidad,@Pais,@CodigoPostal)";
-             Connection connection = new Connection(Connection.Database.Pets);
-             return connection.Response.ErrorFound
-                 ? connection.Response
-                 : connection.FetchData(
-                         query: consulta,
-                         parameters: new Dictionary<string, object> {
-                     { "@CUIT", proveedor.CUIT },
-                     { "@RazonSocial", proveedor.RazonSocial },
-                     { "@NombreContacto", proveedor.NombreContacto },
-                     { "@CorreoElectronico", proveedor.CorreoElectronico },
-                     { "@Telefono", proveedor.Telefono },
-                     { "@Direccion", proveedor.Direccion },
-                     { "@Provincia", proveedor.Provincia },
-                     { "@Localidad", proveedor.Localidad },
-                     { "@Pais", proveedor.Pais },
-                     { "@CodigoPostal", proveedor.CodigoPostal }
-                         }
-                     );
-         }
-         public static Response EliminaProveedor(string CUIT)
-         {
-             string consulta = $"DELETE FROM {Proveedor.Table} WHERE [{Proveedor.Columns.CUIT}] = @cuit";
-             Connection connection = new Connection(Connection.Database.Pets);
-             return connection.Response.ErrorFound
-                 ? connection.Response
-                 : connection.FetchData(
-                         query: consulta,
-                         parameters: new Dictionary<string, object> {
-                             { "@cuit", CUIT }
-                         }
-                     );
-         }
-     }*/
+       
     }
+}
