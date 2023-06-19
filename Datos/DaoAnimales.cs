66606b4ab2800c33f4f18f07243dd13d95f08195
Jean Esquen <jeanpierreesquen@gmail.com>
2023-06-19 01:11:01 -0300

    Jean P1

---
+++ Datos/DaoAnimales.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace Datos
+{
+    public class DaoAnimales
+    {
+        public DaoAnimales() { }
+        private static readonly string ALL_COLUMNS = $"[{Animal.Columns.Codigo}], " +
+                                    $"[{Animal.Columns.Nombre}], " +
+                                    $"[{Animal.Columns.Raza}], ";
+
+        public static class Procedures
+        {
+            public static string Igresar = "SP_IngresarAnimal";
+            public static string Eliminar = "SP_EliminarAnimal";
+            public static string Actializar = "SP_ActualizarAnimales";
+        }
+
+
+        public static Response ObtenerListaDeAnimales()
+        {
+            Connection connection = new Connection(Connection.Database.Pets);
+            return connection.Response.ErrorFound
+                ? connection.Response
+                : connection.FetchData(
+                        query: $"SELECT {ALL_COLUMNS} FROM {Animal.Table}"
+                    );
+        }
+        public static Response BuscarAnimalPorCod(string ID)
+        {
+            string consulta = $"SELECT {ALL_COLUMNS} FROM {Animal.Table} WHERE [{Animal.Columns.Codigo}] = @ID ";
+            Connection connection = new Connection(Connection.Database.Pets);
+            Trace.Write("BuscarAnimalPorCod", $"Consulta: {consulta}");
+            return connection.Response.ErrorFound
+                ? connection.Response
+                : connection.FetchData(
+                        query: consulta,
+                        parameters: new Dictionary<string, object> {
+                            { "@ID", ID }
+                        }
+                    );
+        }
+
+        public static Response IgresarAnimal(Animal An)
+        {
+            Connection con = new Connection(Connection.Database.Pets);
+            return con.Response.ErrorFound
+                ? con.Response
+                : con.ExecuteStoredProcedure(
+                        storedProcedureName: Procedures.Igresar,
+                        parameters: new Dictionary<string, object> {
+                            { "@PK_CodAnimales_An", An.Codigo },
+                            { "@nombre_An", An.Nombre },
+                            { "@NombreDeRaza_An", An.Raza }
+                        }
+                    );
+        }
+
+        public static Response EliminarAnimal(Animal An)
+        {
+            Connection con = new Connection(Connection.Database.Pets);
+            return con.Response.ErrorFound
+                ? con.Response
+                : con.ExecuteStoredProcedure(
+                        storedProcedureName: Procedures.Eliminar,
+                        parameters: new Dictionary<string, object> {
+                            { "@PK_CodAnimales_An", An.Codigo }
+                        }
+                    );
+        }
+
+        public static Response ActualizarAnimal(Animal An)
+        {
+            Connection con = new Connection(Connection.Database.Pets);
+            return con.Response.ErrorFound
+                ? con.Response
+                : con.ExecuteStoredProcedure(
+                        storedProcedureName: Procedures.Actializar,
+                        parameters: new Dictionary<string, object> {
+                            { "@PK_CodAnimales_An", An.Codigo },
+                            { "@nombre_An", An.Nombre },
+                            { "@NombreDeRaza_An", An.Raza }
+                        }
+                    );
+        }
+    }
+}
--- Entidades/Animal.cs
+++ Entidades/Animal.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entidades {
-    class Animal {
-        public int Codigo { get; set; }
+namespace Entidades
+{
+    public class Animal
+    {
+        public static class Columns
+        {
+            public const string Codigo = "PK_CodAnimales_An";
+            public const string Nombre = "nombre_An";
+            public const string Raza = "NombreDeRaza_An";
+        }
+        public const string Table = "Animales";
+        public string Codigo { get; set; }
         public string Nombre { get; set; }
-        public string Descripcion { get; set; }
-        public int Edad { get; set; }
+        public string Raza { get; set; }
     }
 }
 
