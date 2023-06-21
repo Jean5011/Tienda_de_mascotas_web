8a597398bfee1c402f6166efe5b1df1040bdd0d2
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-06-20 22:16:52 -0300

    Carga de datos de prueba

--- Vista/Empleados/CrearCuenta.aspx.cs
+++ Vista/Empleados/CrearCuenta.aspx.cs
@@ -30,6 +30,24 @@ namespace Vista.Empleados {
             Session[actualUser] = res_b.ErrorFound ? null : res_b.ObjectReturned as Empleado;
             return true;
         }
+
+        protected void CargarDatosPrueba() {
+            txtDNI.Text = "45009001";
+            txtNombre.Text = "Derrick";
+            txtApellido.Text = "Dime";
+            ddlGenero.SelectedValue = "M";
+            txtFechaNacimiento.Text = "1990-01-05";
+            txtFechaContrato.Text = "2015-05-01";
+            txtSueldo.Text = "456009";
+            txtDireccion.Text = "Washington 2040";
+            txtLocalidad.Text = "Belgrano R.";
+            txtProvincia.Text = "Ciudad Autónoma de Buenos Aires";
+            txtNacionalidad.Text = "Argentina";
+            txtClave.Text = "tomato";
+            txtConfirmarClave.Text = "tomato";
+            chkAdmin.Checked = true;
+            chkEstado.Checked = true;
+        }
         protected void Page_Load(object sender, EventArgs e) {
             if (!IsPostBack) {
                 bool inicioSesion = CargarSesion();
@@ -42,22 +60,7 @@ namespace Vista.Empleados {
 
                     }
                 }
-                txtDNI.Text = "45009001";
-                txtNombre.Text = "Derrick";
-                txtApellido.Text = "Dime";
-                ddlGenero.SelectedValue = "M";
-                txtFechaNacimiento.Text = "1990-01-05";
-                txtFechaContrato.Text = "2015-05-01";
-                txtSueldo.Text = "456009";
-                txtDireccion.Text = "Washington 2040";
-                txtLocalidad.Text = "Belgrano R.";
-                txtProvincia.Text = "Ciudad Autónoma de Buenos Aires";
-                txtNacionalidad.Text = "Argentina";
-                txtClave.Text = "tomato";
-                txtConfirmarClave.Text = "tomato";
-                chkAdmin.Checked = true;
-                chkEstado.Checked = true;
-
+                CargarDatosPrueba();
             }
         }
 
@@ -80,7 +83,8 @@ namespace Vista.Empleados {
                 Rol = chkAdmin.Checked ? Empleado.Roles.ADMIN : Empleado.Roles.NORMAL
             };
             string claveIngresada = txtClave.Text;
-            SesionNegocio.Autenticar((res) => {
+
+            SesionNegocio.Autenticar((res) => { // Si todo está en orden (El token existe, y está habilitado), mandamos a crear el empleado.
                 Response operacion = EmpleadoNegocio.CrearEmpleado(obj, claveIngresada);
                 if (operacion.ErrorFound) {
                     Utils.MostrarMensaje($"Error. {operacion.Details}.", this.Page, GetType());
@@ -89,7 +93,7 @@ namespace Vista.Empleados {
                 else {
                     Utils.MostrarMensaje("El empleado fue registrado exitosamente. ", this.Page, GetType());
                 }
-            }, (err) => {
+            }, (err) => { // Si hubo un error con el token, no hacemos nada.
                 Utils.MostrarMensaje("Caducó el token de sesión. Volvé a iniciar sesión.", this.Page, GetType());
             });
             
