c4dad666fce62365dfa7f56b40db82962bba1fe6
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-06-26 17:55:36 -0300

    Diseño tabla Productos

--- Vista/Productos/Administrar.aspx.cs
+++ Vista/Productos/Administrar.aspx.cs
@@ -24,19 +24,19 @@ namespace Vista.Productos {
             string et = txtBuscar.Text;
             Response response = string.IsNullOrEmpty(et) ? ProdN.ObtenerProductos() : ProdN.ObtenerPorCod(et);
             if (!response.ErrorFound) {
-                grdProductos.DataSource = (DataSet)response.ObjectReturned;
-                grdProductos.DataBind();
+                gvDatos.DataSource = (DataSet)response.ObjectReturned;
+                gvDatos.DataBind();
             }
 
 
 
         }
         protected void GrdProductos_RowEditing(object sender, GridViewEditEventArgs e) {
-            grdProductos.EditIndex = e.NewEditIndex;
+            gvDatos.EditIndex = e.NewEditIndex;
             CargarGridView();
         }
         protected void GrdProductos_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e) {
-            grdProductos.EditIndex = -1;
+            gvDatos.EditIndex = -1;
             CargarGridView();
         }
         protected bool EsAdmin() {
@@ -45,14 +45,14 @@ namespace Vista.Productos {
             return UsuarioActual.Rol == Empleado.Roles.ADMIN;
         }
         protected void GrdProductos_RowUpdating(object sender, GridViewUpdateEventArgs e) {
-            String Cod = ((Label)grdProductos.Rows[e.RowIndex].FindControl("lbl_eit_codigo")).Text;
-            String Prov = ((TextBox)grdProductos.Rows[e.RowIndex].FindControl("txt_eit_Prov")).Text;
-            String Tipo = ((TextBox)grdProductos.Rows[e.RowIndex].FindControl("txt_eit_Tipo")).Text;
-            String Nombre = ((TextBox)grdProductos.Rows[e.RowIndex].FindControl("txt_eit_Nombre")).Text;
-            String Marca = ((TextBox)grdProductos.Rows[e.RowIndex].FindControl("txt_eit_Marca")).Text;
-            String Desc = ((TextBox)grdProductos.Rows[e.RowIndex].FindControl("txt_eit_Desc")).Text;
-            String Stck = ((TextBox)grdProductos.Rows[e.RowIndex].FindControl("txt_eit_Stock")).Text;
-            String Precio = ((TextBox)grdProductos.Rows[e.RowIndex].FindControl("txt_eit_Precio")).Text;
+            String Cod = ((Label)gvDatos.Rows[e.RowIndex].FindControl("lbl_eit_codigo")).Text;
+            String Prov = ((TextBox)gvDatos.Rows[e.RowIndex].FindControl("txt_eit_Prov")).Text;
+            String Tipo = ((TextBox)gvDatos.Rows[e.RowIndex].FindControl("txt_eit_Tipo")).Text;
+            String Nombre = ((TextBox)gvDatos.Rows[e.RowIndex].FindControl("txt_eit_Nombre")).Text;
+            String Marca = ((TextBox)gvDatos.Rows[e.RowIndex].FindControl("txt_eit_Marca")).Text;
+            String Desc = ((TextBox)gvDatos.Rows[e.RowIndex].FindControl("txt_eit_Desc")).Text;
+            String Stck = ((TextBox)gvDatos.Rows[e.RowIndex].FindControl("txt_eit_Stock")).Text;
+            String Precio = ((TextBox)gvDatos.Rows[e.RowIndex].FindControl("txt_eit_Precio")).Text;
             //bool Estado = ((CheckBox)grdProductos.Rows[e.RowIndex].FindControl("chk_eit_estado")).Checked;
 
 
@@ -82,14 +82,14 @@ namespace Vista.Productos {
                     SesionNegocio.Autenticar((res) => {
                         Response response = ProductoNegocio.ActualizarProducto(prod);
                         if (!response.ErrorFound) {
-                            grdProductos.EditIndex = -1;
+                            gvDatos.EditIndex = -1;
                             CargarGridView();
                             Utils.MostrarMensaje("Se actualizó correctamente el producto. ", this.Page, GetType());
                             //Mostrar cartel de producto actualizado correctamente
                         }
                         else {
                             Utils.MostrarMensaje(response.Message, this.Page, GetType());
-                            grdProductos.EditIndex = -1;
+                            gvDatos.EditIndex = -1;
                         }
                     }, (err) => {
                         Utils.MostrarMensaje("Venció el token. Volvé a iniciar sesión para continuar. ", this.Page, GetType());
@@ -98,14 +98,14 @@ namespace Vista.Productos {
                     CargarGridView();
                 }
                 else {
-                    grdProductos.EditIndex = -1;
+                    gvDatos.EditIndex = -1;
                     CargarGridView();
                     Utils.MostrarMensaje("Error al convertir Stock. ", this.Page, GetType());
                     //NO SE PUDO CONVERTIR EL STOCK A INT, MOSTRAR MENSAJE DE ERROR "STOCK INGRESADO NO VALIDO"
                 }
             }
             else {
-                grdProductos.EditIndex = -1;
+                gvDatos.EditIndex = -1;
                 CargarGridView();
                 Utils.MostrarMensaje("Error al convertir Precio. ", this.Page, GetType());
                 // No se pudo convertir a double. Mostrar mensaje de error
@@ -119,7 +119,7 @@ namespace Vista.Productos {
                 });
                 return;
             }
-            String Cod = ((Label)grdProductos.Rows[e.RowIndex].FindControl("lbl_it_codigo")).Text;
+            String Cod = ((Label)gvDatos.Rows[e.RowIndex].FindControl("lbl_it_codigo")).Text;
 
             Producto prod = new Producto {
                 Codigo = Cod
@@ -134,12 +134,58 @@ namespace Vista.Productos {
             }
         }
         protected void GrdProductos_PageIndexChanging(object sender, GridViewPageEventArgs e) {
-            grdProductos.PageIndex = e.NewPageIndex;
+            gvDatos.PageIndex = e.NewPageIndex;
             CargarGridView();
-        }
+        } 
         protected void BtnBuscar_Click(object sender, EventArgs e) {
             CargarGridView();
         }
+
+
+        public void CargarDatos() {
+            CargarGridView();
+        }
+        protected void GvDatos_PageIndexChanging(object sender, GridViewPageEventArgs e) {
+            gvDatos.PageIndex = e.NewPageIndex;
+            CargarDatos();
+        }
+
+        protected void GvDatos_RowCreated(object sender, GridViewRowEventArgs e) {
+            if (e.Row.RowType == DataControlRowType.Pager) {
+                if (e.Row.FindControl("gvDatosPagerPageTxtBox") is TextBox txtPagerTextBox) {
+                    txtPagerTextBox.Text = (gvDatos.PageIndex + 1) + "";
+                }
+                if (e.Row.FindControl("ddlFilasPorPaginaPagerTemplate") is DropDownList ddlPager) {
+                    ddlPager.SelectedValue = gvDatos.PageSize + "";
+                }
+            }
+        }
+        protected void GvDatosPagerPageTxtBox_TextChanged(object sender, EventArgs e) {
+            int intendedPage = int.Parse(((TextBox)sender).Text) - 1;
+            if (intendedPage <= gvDatos.PageCount - 1) {
+                gvDatos.PageIndex = intendedPage;
+                CargarDatos();
+            }
+            else {
+                ((TextBox)sender).Text = gvDatos.PageIndex + "";
+            }
+        }
+
+        protected void DdlFilasPorPaginaPagerTemplate_SelectedIndexChanged(object sender, EventArgs e) {
+            int filasPorPaginaN = int.Parse(((DropDownList)sender).SelectedValue);
+            if (filasPorPaginaN > 0) {
+                gvDatos.PageSize = filasPorPaginaN;
+                CargarDatos();
+            }
+        }
+
+        protected void GvDatos_SelectedIndexChanging(object sender, GridViewSelectEventArgs e) {
+
+        }
+
+
+
+
     }
 
 
