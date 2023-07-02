6329da8843e80cfc27173de16241d5ae8bd90776
Jean Esquen <jeanpierreesquen@gmail.com>
2023-07-02 16:15:19 -0300

    Jean d2

--- LAB3/base pets.sql
+++ LAB3/base pets.sql
@@ -41,7 +41,8 @@ go
 
 CREATE TABLE Proveedores
 (
-CUIT_Prov varchar(10) NOT NULL,RazonSocial_Prov varchar(50) NOT NULL,
+CUIT_Prov varchar(10) NOT NULL,
+RazonSocial_Prov varchar(50) NOT NULL,
 NombreDeContacto_Prov varchar(30) NOT NULL,
 CorreoElectronico_Prov varchar(75) NOT NULL,
 Telefono_Prov varchar(20) NOT NULL,
@@ -53,21 +54,82 @@ CodigoPostal_Prov varchar(10) NOT NULL,
 Estado_Prov bit NOT NULL DEFAULT 1,
 CONSTRAINT PK_Proveedores PRIMARY KEY(CUIT_Prov)
 )
---insertar 10 registros
+go
+---- tABLAS CREADAS POR MÁXIMO CANEDO ----
 
-insert into Proveedores (CUIT_Prov,RazonSocial_Prov,NombreDeContacto_Prov,CorreoElectronico_Prov,Telefono_Prov,Dirreccion_Prod,Provincia_Prod,Localidad_Prod,Pais_Prod,CodigoPostal_Prov)
-select '1234567890', 'Proveedor A', 'Juan Pérez', 'juanperez@gmail.com', '123456789', 'Calle 123', 'Buenos Aires', 'Ciudad Autónoma', 'Argentina', '1234' union
-select '0987654321', 'Proveedor B', 'María López', 'marialopez@hotmail.com', '987654321', 'Avenida 456', 'Córdoba', 'Córdoba', 'Argentina', '5678' union 
-select '1112223334', 'Proveedor C', 'Pedro Gómez', 'pedrogomez@hotmail.com', '111222333', 'Plaza 789', 'Santa Fe', 'Rosario', 'Argentina', '9012' union 
-select '5556667778', 'Proveedor D', 'Ana Rodríguez', 'anarodriguez@hotmail.com', '555666777', 'Carretera 012', 'Mendoza', 'Mendoza', 'Argentina', '3456' union
-select '9998887776', 'Proveedor E', 'Luis Torres', 'luistorres@outlook.com', '999888777', 'Ruta 345', 'Salta', 'Salta', 'Argentina', '7890' union
-select '4443332220', 'Proveedor F', 'Carolina Martínez', 'carolinamartinez@gmail.com', '444333222', 'Camino 567', 'Tucumán', 'San Miguel de Tucumán', 'Argentina', '2345' union
-select '7778889992', 'Proveedor G', 'Andrés Castro', 'andrescastro@gmail.com', '777888999', 'Autopista 890', 'Chaco', 'Resistencia', 'Argentina', '6789' union 
-select '6665554443', 'Proveedor H', 'Mónica Herrera', 'monicaherrera@gmail.com', '666555444', 'Paseo 123', 'Entre Ríos', 'Paraná', 'Argentina', '0123' union 
-select '2223334445', 'Proveedor I', 'Roberto Sánchez', 'robertosanchez@outlook.com', '222333444', 'Pasaje 456', 'Neuquén', 'Neuquén', 'Argentina', '4567' union
-select '8887776667', 'Proveedor J', 'Marcela Fernández', 'marcelafernandez@hotmail.com', '888777666', 'Galería 789', 'San Juan', 'San Juan', 'Argentina', '8901'
+CREATE TABLE Empleados (
+	DNI_Em CHAR(12) NOT NULL,
+	Nombre_Em VARCHAR(48) NOT NULL,
+	Apellido_Em VARCHAR(48),
+	Sexo_Em CHAR(2) NOT NULL DEFAULT 'M',
+	FechaDeNacimiento_Em DATE NOT NULL,
+	FechaDeInicio_Em DATE,
+	Sueldo_Em MONEY,
+	Direccion_Em VARCHAR(48),
+	Provincia_Em VARCHAR(48),
+	Localidad_Em VARCHAR(48),
+	Nacionalidad_Em VARCHAR(48),
+	Estado_Em BIT NOT NULL DEFAULT 1,
+	Hash_Em VARCHAR(256) NOT NULL,
+	Salt_Em VARCHAR(256) NOT NULL,
+	CONSTRAINT PK_DNI_Em PRIMARY KEY (DNI_Em)
+)
+GO
+
+---- tABLAS CREADAS POR EZEQUIEL ALEJANDRO MARTINEZ ----
+
+Create Table Productos
+(
+CodProducto_Prod varchar(10) not null,
+CUITProveedor_Prod varchar(10) not null,
+CodTipoProducto_Prod varchar(10) not null,
+Nombre_Prod varchar(50) not null,
+Marca_Prod varchar(50) not null,
+Descripcion_Prod varchar(50) not null,
+Stock_Prod int not null,
+Imagen_Prod varchar(100) null,
+PrecioUnitario_Prod money not null,
+Estado_Prod bit default 1
+CONSTRAINT PK_Productos PRIMARY KEY (CodProducto_Prod,CUITProveedor_Prod),
+
+CONSTRAINT FK_Productos_CUIT FOREIGN KEY (CUITProveedor_Prod) 
+references Proveedores (CUIT_Prov),
+
+CONSTRAINT FK_Productos_TIPO FOREIGN KEY (CodTipoProducto_Prod)
+references TipoDeProductos (PK_CodTipoProducto_TP)
+
+)
 go
 
----- tABLAS CREADAS POR MÁXIMO CANEDO ----
+Create Table Ventas
+(
+CodVenta_Vt int identity(1,1),
+DNIEmpleado_Vt char(12) not null,
+TipoPago_Vt varchar(50) not null,
+Fecha_Vt Date not null,
+PrecioTotal_Vt money not null,
+CONSTRAINT PK_Ventas PRIMARY KEY (CodVenta_Vt),
+
+CONSTRAINT FK_Ventas_DNI FOREIGN KEY (DNIEmpleado_Vt)
+references Empleados (DNI_Em)
+)
+go
+
+---- tABLAS CREADAS POR MARÍA OLIVIA HANCZYC  ----
+
+CREATE TABLE DetalleDeVenta
+(
+CodVenta_Dv int NOT NULL,
+CodProducto_Dv varchar(10) NOT NULL,
+CUITProveedor_Dv varchar(15) NOT NULL,
+Cantidad_Dv int NOT NULL,
+PrecioUnitario_Dv money NOT NULL,
+PrecioTotal_Dv money NOT NULL,
+Estado_Prod bit default 1,
+CONSTRAINT PK_DetalleDeVenta PRIMARY KEY (CodVenta_Dv, CodProducto_Dv, CUITProveedor_Dv),
+CONSTRAINT FK_DetalleDeVenta_Ventas FOREIGN KEY (CodVenta_Dv) REFERENCES Ventas (CodVenta_Vt),
+CONSTRAINT FK_DetalleDeVenta_Productos FOREIGN KEY (CodProducto_Dv, CUITProveedor_Dv) REFERENCES Productos (CodProducto_Prod, CUITProveedor_Prod)
+)
+GO
 
 select  upper('Javier Andres Torales'), upper('María Olivia Hanczyc '),upper('Ezequiel Alejandro Martinez'),upper('Máximo Canedo')
