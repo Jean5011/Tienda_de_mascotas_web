bde3d2d2b2587c5ce875a12f0a465a9889de964b
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-07-02 19:24:36 -0300

    Formato

--- Vista/Proveedores/Editar.aspx.cs
+++ Vista/Proveedores/Editar.aspx.cs
@@ -9,14 +9,10 @@ using Negocio;
 using Entidades;
 
 
-namespace Vista.Proveedores
-{
-    public partial class Editar : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            if (!IsPostBack)
-            {
+namespace Vista.Proveedores {
+    public partial class Editar : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            if (!IsPostBack) {
                 Session[Utils.AUTH] = AuthorizationVista.ValidateSession(this, Authorization.ONLY_EMPLOYEES_STRICT);
 
                 var auth = Session[Utils.AUTH] as SessionData;
@@ -26,76 +22,68 @@ namespace Vista.Proveedores
                 //{
                 // string CUIT = Request.QueryString["cuit"];
                 string CUIT = "777777777777";//proveedor de testeo
-                    
 
-                    Response response = ProveedorNegocio.ObtenerProveedorCUITEditar(CUIT);
-                    if (!response.ErrorFound)
-                    {
-                        DataSet ds = response.ObjectReturned as DataSet;
-                        if (ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
-                        {
-                            DataRow proveedorRow = ds.Tables[0].Rows[0];
 
-                            Cuit_tb.Text = proveedorRow["CUIT_Prov"].ToString();
-                            RazonSocial_tb.Text = proveedorRow["RazonSocial_Prov"].ToString();
-                            NombreContacto_tb.Text = proveedorRow["NombreDeContacto_Prov"].ToString();
-                            CorreoElectronico_tb.Text = proveedorRow["CorreoElectronico_Prov"].ToString(); 
-                            NumeroTelefono_tb.Text = proveedorRow["Telefono_Prov"].ToString();
-                            Direccion_tb.Text = proveedorRow["Direccion_Prov"].ToString();
-                            Provincia_tb.Text = proveedorRow["Provincia_Prov"].ToString();
-                            localidad_tb.Text = proveedorRow["Localidad_Prov"].ToString();
-                            Pais_tb.Text = proveedorRow["Pais_Prov"].ToString();
-                            CodigoPostal_tb.Text = proveedorRow["CodigoPostal_Prov"].ToString();
-                            //estado check box =  proveedorRow["Estado_Prov"].ToString();
+                Response response = ProveedorNegocio.ObtenerProveedorCUITEditar(CUIT);
+                if (!response.ErrorFound) {
+                    DataSet ds = response.ObjectReturned as DataSet;
+                    if (ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0) {
+                        DataRow proveedorRow = ds.Tables[0].Rows[0];
 
-                          
-                        }
-                        else
-                        {
-                            
-                            Utils.MostrarMensaje($"No se encontro el CUIT ", this.Page, GetType());
-                        }
+                        Cuit_tb.Text = proveedorRow["CUIT_Prov"].ToString();
+                        RazonSocial_tb.Text = proveedorRow["RazonSocial_Prov"].ToString();
+                        NombreContacto_tb.Text = proveedorRow["NombreDeContacto_Prov"].ToString();
+                        CorreoElectronico_tb.Text = proveedorRow["CorreoElectronico_Prov"].ToString();
+                        NumeroTelefono_tb.Text = proveedorRow["Telefono_Prov"].ToString();
+                        Direccion_tb.Text = proveedorRow["Direccion_Prov"].ToString();
+                        Provincia_tb.Text = proveedorRow["Provincia_Prov"].ToString();
+                        localidad_tb.Text = proveedorRow["Localidad_Prov"].ToString();
+                        Pais_tb.Text = proveedorRow["Pais_Prov"].ToString();
+                        CodigoPostal_tb.Text = proveedorRow["CodigoPostal_Prov"].ToString();
+                        //estado check box =  proveedorRow["Estado_Prov"].ToString();
+
+
                     }
-                    else
-                    {
-                        Response.Redirect("Administrar.aspx");
-                        Utils.MostrarMensaje($"error ", this.Page, GetType());
+                    else {
+
+                        Utils.MostrarMensaje($"No se encontro el CUIT ", this.Page, GetType());
                     }
+                }
+                else {
+                    Response.Redirect("Administrar.aspx");
+                    Utils.MostrarMensaje($"error ", this.Page, GetType());
+                }
                 //}
             }
         }
 
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-            
-                    Proveedor proveedor = new Proveedor
-                    {
-                        CUIT = Cuit_tb.Text,
-                        RazonSocial = RazonSocial_tb.Text,
-                        NombreContacto = NombreContacto_tb.Text,
-                        CorreoElectronico = CorreoElectronico_tb.Text,
-                        Telefono = NumeroTelefono_tb.Text,
-                        Direccion = Direccion_tb.Text,
-                        Provincia = Provincia_tb.Text,
-                        Localidad = localidad_tb.Text,
-                        Pais = Pais_tb.Text,
-                        CodigoPostal = CodigoPostal_tb.Text
-                    };
+        protected void Button1_Click(object sender, EventArgs e) {
 
-                    
-                    Response resActualizarProveedor = ProveedorNegocio.ActualizarProveedor(proveedor);
-                    if (!resActualizarProveedor.ErrorFound)
-                    {
-                        
-                        Utils.ShowSnackbar("Proveedor ha sido actualizado correctamente!.", this, GetType());
+            Proveedor proveedor = new Proveedor {
+                CUIT = Cuit_tb.Text,
+                RazonSocial = RazonSocial_tb.Text,
+                NombreContacto = NombreContacto_tb.Text,
+                CorreoElectronico = CorreoElectronico_tb.Text,
+                Telefono = NumeroTelefono_tb.Text,
+                Direccion = Direccion_tb.Text,
+                Provincia = Provincia_tb.Text,
+                Localidad = localidad_tb.Text,
+                Pais = Pais_tb.Text,
+                CodigoPostal = CodigoPostal_tb.Text
+            };
 
-                    }
-                    else
-                    {
-                        Utils.ShowSnackbar("Hubo un error, no se pudo actualizar el proveedor.", this, GetType());
-                    }
-                    //Utils.MostrarMensaje($"El proveedor se agregaria correctamente en este caso. ", this.Page, GetType());//ELIMINAR ESTO, ES UN MENSAJE DE TESTEO AL VERIFICAR
+
+            Response resActualizarProveedor = ProveedorNegocio.ActualizarProveedor(proveedor);
+            if (!resActualizarProveedor.ErrorFound) {
+
+                Utils.ShowSnackbar("Proveedor ha sido actualizado correctamente!.", this, GetType());
+
+            }
+            else {
+                Utils.ShowSnackbar("Hubo un error, no se pudo actualizar el proveedor.", this, GetType());
+            }
+            //Utils.MostrarMensaje($"El proveedor se agregaria correctamente en este caso. ", this.Page, GetType());//ELIMINAR ESTO, ES UN MENSAJE DE TESTEO AL VERIFICAR
         }
-        
+
     }
 }
