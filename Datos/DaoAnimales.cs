68ce62f2b0dc4c9bfe597b335ad206569a1e0892
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-07-04 10:48:06 -0300

    #30 #32 Documentación y ligeros cambios.

--- Datos/DaoAnimales.cs
+++ Datos/DaoAnimales.cs
@@ -8,26 +8,36 @@ using System.Text;
 using System.Threading.Tasks;
 using Entidades;
 
-namespace Datos
-{
-    public class DaoAnimales
-    {
+namespace Datos {
+    public class DaoAnimales {
         public DaoAnimales() { }
-        private static readonly string ALL_COLUMNS = $"[{Animal.Columns.Codigo}], " +
-                                    $"[{Animal.Columns.Nombre}], " +
-                                    $"[{Animal.Columns.Raza}]";
 
-        public static class Procedures
-        {
-            public static string Igresar = "SP_IngresarAnimal";
+        /// <summary>
+        /// Todas las columnas de la tabla Animales.
+        /// </summary>
+        private static string ALL_COLUMNS {
+            get {
+                return $"[{Animal.Columns.Codigo}], " +
+                        $"[{Animal.Columns.Nombre}], " +
+                        $"[{Animal.Columns.Raza}]";
+            }
+        }
+
+        /// <summary>
+        /// Listado de procedimientos que se utilizan en esta clase.
+        /// </summary>
+        public static class Procedures {
+            public static string Ingresar = "SP_IngresarAnimal";
             public static string Eliminar = "SP_EliminarAnimal";
             public static string Actializar = "SP_ActualizarAnimales";
             public static string Alta = "SP_AltaAnimal";
         }
 
-
-        public static Response ObtenerListaDeAnimales()
-        {
+        /// <summary>
+        /// Obtener tabla de animales activos.
+        /// </summary>
+        /// <returns>Objeto Response con el resultado de la operación.</returns>
+        public static Response ObtenerListaDeAnimales() {
             Connection connection = new Connection(Connection.Database.Pets);
             return connection.Response.ErrorFound
                 ? connection.Response
@@ -36,8 +46,11 @@ namespace Datos
                     );
         }
 
-        public static Response ObtenerLista()
-        {
+        /// <summary>
+        /// Obtener lista de animales activos. (Redundancia)
+        /// </summary>
+        /// <returns>Objeto Response con el resultado de la operación.</returns>
+        public static Response ObtenerLista() {
             Connection connection = new Connection(Connection.Database.Pets);
             return connection.Response.ErrorFound
                 ? connection.Response
@@ -45,8 +58,13 @@ namespace Datos
                         query: $"SELECT {ALL_COLUMNS} FROM {Animal.Table} where {Animal.Columns.Estado}=1"
                     );
         }
-        public static Response BuscarAnimalPorCod(string ID)
-        {
+
+        /// <summary>
+        /// Buscar animal activo cuyo ID coincida con uno dado.
+        /// </summary>
+        /// <param name="ID">ID a buscar.</param>
+        /// <returns>Objeto Response con el resultado de la operación.</returns>
+        public static Response BuscarAnimalPorCod(string ID) {
             string consulta = $"SELECT {ALL_COLUMNS} FROM {Animal.Table} WHERE [{Animal.Columns.Codigo}] = @ID and {Animal.Columns.Estado}=1 ";
             Connection connection = new Connection(Connection.Database.Pets);
             Trace.Write("BuscarAnimalPorCod", $"Consulta: {consulta}");
@@ -60,13 +78,17 @@ namespace Datos
                     );
         }
 
-        public static Response IgresarAnimal(Animal An)
-        {
+        /// <summary>
+        /// Agrega un registro Animal a la base de datos.
+        /// </summary>
+        /// <param name="An">Objeto Animal con los datos a ingresar.</param>
+        /// <returns>Objeto Response con el resultado de la transacción.</returns>
+        public static Response IgresarAnimal(Animal An) {
             Connection con = new Connection(Connection.Database.Pets);
             return con.Response.ErrorFound
                 ? con.Response
                 : con.ExecuteStoredProcedure(
-                        storedProcedureName: Procedures.Igresar,
+                        storedProcedureName: Procedures.Ingresar,
                         parameters: new Dictionary<string, object> {
                             { "@PK_CodAnimales_An", An.Codigo },
                             { "@nombre_An", An.Nombre },
@@ -75,8 +97,12 @@ namespace Datos
                     );
         }
 
-        public static Response EliminarAnimal(Animal An)
-        {
+        /// <summary>
+        /// Elimina un animal de la base de datos. 
+        /// </summary>
+        /// <param name="An">Objeto Animal con el código establecido.</param>
+        /// <returns>Objeto Response con el resultado de la operación.</returns>
+        public static Response EliminarAnimal(Animal An) {
             Connection con = new Connection(Connection.Database.Pets);
             return con.Response.ErrorFound
                 ? con.Response
@@ -88,8 +114,12 @@ namespace Datos
                     );
         }
 
-        public static Response ActualizarAnimal(Animal An)
-        {
+        /// <summary>
+        /// Actualiza un registro Animal en la base de datos con los datos de un objeto Animal dado.
+        /// </summary>
+        /// <param name="An">Objeto Animal con los datos a actualizar.</param>
+        /// <returns>Objeto Response con el resultado de la transacción.</returns>
+        public static Response ActualizarAnimal(Animal An) {
             Connection con = new Connection(Connection.Database.Pets);
             return con.Response.ErrorFound
                 ? con.Response
@@ -103,9 +133,11 @@ namespace Datos
                     );
         }
 
-        /*************************************************************************************************************************************************/
-        public static Response ObtenerListaBaja()
-        {
+        /// <summary>
+        /// Obtener lista de animales inactivos.
+        /// </summary>
+        /// <returns>Objeto Response con el resultado de la operación.</returns>
+        public static Response ObtenerListaBaja() {
             Connection connection = new Connection(Connection.Database.Pets);
             return connection.Response.ErrorFound
                 ? connection.Response
@@ -113,8 +145,13 @@ namespace Datos
                         query: $"SELECT {ALL_COLUMNS} FROM {Animal.Table} where {Animal.Columns.Estado}=0"
                     );
         }
-        public static Response BuscarAnimalPorCodBaja(string ID)
-        {
+
+        /// <summary>
+        /// Buscar animal inactivo cuyo ID coincida con uno dado.
+        /// </summary>
+        /// <param name="ID">ID a buscar.</param>
+        /// <returns>Objeto Response con el resultado de la operación.</returns>
+        public static Response BuscarAnimalPorCodBaja(string ID) {
             string consulta = $"SELECT {ALL_COLUMNS} FROM {Animal.Table} WHERE [{Animal.Columns.Codigo}] = @ID and {Animal.Columns.Estado}=0 ";
             Connection connection = new Connection(Connection.Database.Pets);
             Trace.Write("BuscarAnimalPorCod", $"Consulta: {consulta}");
@@ -128,8 +165,12 @@ namespace Datos
                     );
         }
 
-        public static Response AltaAnimal(Animal An)
-        {
+        /// <summary>
+        /// Da de alta un registro animal en la base de datos a partir de su código.
+        /// </summary>
+        /// <param name="An">Objeto Animal con el código establecido.</param>
+        /// <returns>Objeto Response con el resultado de la transacción.</returns>
+        public static Response AltaAnimal(Animal An) {
             Connection con = new Connection(Connection.Database.Pets);
             return con.Response.ErrorFound
                 ? con.Response
@@ -142,4 +183,4 @@ namespace Datos
         }
 
     }
-    }
+}
