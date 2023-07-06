12cc78bf61689f00bc5df45a3c7debe7756a91a4
javier torales <jtorales2016@gmail.com>
2023-07-06 08:38:49 -0300

    se traslado logiga de vista a negocio

--- Negocio/ProveedorNegocio.cs
+++ Negocio/ProveedorNegocio.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Entidades;
 using Datos;
 using System.Text.RegularExpressions;
+using System.Data;
 
 namespace Negocio
 {
@@ -21,11 +22,40 @@ namespace Negocio
         }
         public static Response InsertarProveedor(Proveedor proveedor)
         {
-            return ProveedorDatos.InsertarProveedor(proveedor);
+            Response response = new Response();
+            Response existe = ProveedorDatos.VerificarExiste(proveedor.CUIT);
+            
+            if (!existe.ErrorFound)
+            {
+                int cantidad;
+                DataSet dt = existe.ObjectReturned as DataSet;
+                cantidad = Convert.ToInt32(dt.Tables[0].Rows[0]["CUIT"]);
+                if (cantidad > 0)
+                {   //si cantidad es mayor a cero es pq ya existe
+                    response.ErrorFound = true;
+                    response.Message = " El CUIT de proveedor ingresado ya existe.";
+                }
+                else
+                {   //si es igual a cero es pq no existe, se crea el proveedor
+                    Response resInsertarProveedor = ProveedorDatos.InsertarProveedor(proveedor);
+                    if (!resInsertarProveedor.ErrorFound)
+                    {
+                        response.ErrorFound = false;
+                        response.Message = "Proveedor ha sido agregado correctamente!.";
+                    }
+                    else
+                    {
+                        response.ErrorFound = true;
+                        response.Message = "Hubo un error, no se pudo agregar el proveedor.";
+                    }
+                }
+            }
+
+            return response;
         }
         public static Response EliminadoLogicoProveedor(String CUIT)
         {
-            return ProveedorDatos.EliminadoLogicoProveedor(new Proveedor() {CUIT=CUIT,Estado="0"});
+            return ProveedorDatos.EliminadoLogicoProveedor(new Proveedor() { CUIT = CUIT, Estado = "0" });
         }
         public static Response ActualizarProveedor(Proveedor proveedor)
         {
